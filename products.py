86b786a812f47f5579846ef1fbb5b0df00762b06
Sing Mok <singmok@ymail.com>
2022-02-23 14:59:56 +0800

    updata-5

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
-# 讀取檔案
+import os
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+
+# 檢查檔案和讀取檔案
+if os.path.isfile('products.csv'):
+    print('Yes')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('No')
 
 # 讓使用者輸入
 while True:
